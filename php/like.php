5938c65b8451563db4762ccd794a4b15ba3cd16f
Louis Maupas <louismaupas@gmail.com>
2020-07-12 19:09:37 +0200

    add vote systm 2.0 intégration du type de vote (like or dislike)

--- php/like.php
+++ php/like.php
@@ -3,7 +3,7 @@ if (!(isset($_SESSION['question']) && $_SESSION['question'] != ''))
 {
     header ("Location: login.php");
 }
-echo 'Session : ' . $_SESSION['username'] . "  actor cest " . $_GET['actoris'] . "   " . $_GET['vote'];
+echo 'Session : ' . $_SESSION['username'] . " LE ID de l'user est "  . $_SESSION['id_user'] . "  L'ACTEUR EST " . $_GET['actoris'] . " LE VOTE EST  " . $_GET['vote'];
 
 //connexion à la BDD
 $bdd = new PDO('mysql:hostname=localhost;dbname=projettroisbdd','root','');
@@ -14,21 +14,32 @@ if(isset($_GET['vote'],$_GET['actoris']) AND !empty($_GET['vote']) AND !empty($_
     $getActor = (int) $_GET['actoris'];
     $check = $bdd->prepare('SELECT id_vote FROM vote WHERE id_actor = ? AND id_user = ?');
     $check->execute(array($getVote,($_SESSION['id_user'])));
+    //les varaibles des marqueurs à lier
+    $voteLike = 1;
+    $voteDislike = 2;
     // si c'est un like
        if($getVote == 1) {
-        $insert = $bdd->prepare('INSERT INTO vote (id_actor, id_user) VALUES (?, ?)');
-        $insert->execute(array($getActor, $_SESSION['id_user']));   
+        $insert = $bdd->prepare('INSERT INTO vote (id_actor, id_user, choice) VALUES (?, ?, ?)');
+        $insert->bindValue(1, $getActor, PDO::PARAM_INT);
+        $insert->bindValue(2, $_SESSION['id_user'], PDO::PARAM_INT);
+        $insert->bindValue(3, $voteLike, PDO::PARAM_INT);   
+        //éxécution de la requete préparée 
+        $insertExecute = $insert->execute();
     // si c'est un dislike       
        } elseif($getVote == 2) {
-        $insert = $bdd->prepare('INSERT INTO vote (id_actor, id_user) VALUES (?, ?)');
-        $insert->execute(array($getActor, $_SESSION['id_user']));
+        $insert = $bdd->prepare('INSERT INTO vote (id_actor, id_user, choice) VALUES (?, ?, ?)');
+        $insert->bindValue(1, $getActor, PDO::PARAM_INT);
+        $insert->bindValue(2, $_SESSION['id_user'], PDO::PARAM_INT);
+        $insert->bindValue(3, $voteDislike, PDO::PARAM_INT);   
+        //éxécution de la requete préparée 
+        $insertExecute = $insert->execute();   
+    // si c'est un dislike        
         }
        /* header('Location: ../html/actorDSA.php');*/
        } else {
            exit('erreur');
         }
-
-        
+       
 ?>
 
 
